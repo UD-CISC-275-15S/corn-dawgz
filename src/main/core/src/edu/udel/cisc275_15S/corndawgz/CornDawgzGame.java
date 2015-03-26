c339e964ad6e7a5dc9b8da753b55b0f4aaec8b4d
Joshua <Joshua@128.4.58.70>
2015-03-26 12:57:46 -0400

    adding the thing

---
+++ src/main/core/src/edu/udel/cisc275_15S/corndawgz/CornDawgzGame.java
@@ -0,0 +1,12 @@
+package edu.udel.cisc275_15S.corndawgz;
+
+import com.badlogic.gdx.Game;
+
+public class CornDawgzGame extends Game{
+	
+	@Override
+	public void create(){
+		setScreen(new StartScreen(this));
+	}
+
+}
