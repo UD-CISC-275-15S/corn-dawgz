d23960113c8072d516550cfddef0a1173f3d3133
Joshua <Joshua@128.4.58.70>
2015-03-26 13:31:37 -0400

    SLAM

--- src/main/core/src/edu/udel/cisc275_15S/corndawgz/CornDawgzGame1.java
+++ src/main/core/src/edu/udel/cisc275_15S/corndawgz/CornDawgzGame1.java
@@ -2,6 +2,8 @@ package edu.udel.cisc275_15S.corndawgz;
 
 import com.badlogic.gdx.Game;
 
+// GITHUB WORK DAMN YOU
+
 public class CornDawgzGame1 extends Game {
 	@Override
 	public void create () {
