67105cecebecf4c5555b6221477792011afbf937
evanshone7 <EvanShone@wifi-roaming-128-4-233-97.host.udel.edu>
2015-03-26 13:29:16 -0400

    Change

--- src/main/core/src/edu/udel/cisc275_15S/corndawgz/CornDawgzGame1.java
+++ src/main/core/src/edu/udel/cisc275_15S/corndawgz/CornDawgzGame1.java
@@ -3,6 +3,7 @@ package edu.udel.cisc275_15S.corndawgz;
 import com.badlogic.gdx.Game;
 
 public class CornDawgzGame1 extends Game {
+	// change
 	@Override
 	public void create () {
 		setScreen(new StartScreen(this));
