df7de273bfc21904ea49c8eb9f428e928b99bcc3
evanshone7 <EvanShone@wifi-roaming-128-4-237-247.host.udel.edu>
2015-04-28 15:18:20 -0400

    Added LocationSelectorScreen

---
+++ src/main/core/src/edu/udel/cisc275_15S/corndawgz/LocationSelectorScreen.java
@@ -0,0 +1,11 @@
+package edu.udel.cisc275_15S.corndawgz;
+
+import com.badlogic.gdx.Game;
+
+public class LocationSelectorScreen extends GameScreen {
+
+	public LocationSelectorScreen(Game g) {
+		super(g);
+	}
+
+}
