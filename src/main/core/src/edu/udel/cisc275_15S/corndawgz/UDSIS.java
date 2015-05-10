12701d4ec6eb9a4b65934c9f4a577435e85612a4
Jake <Jake@128.4.196.194>
2015-05-10 14:37:44 -0400

    Added more to UDSIS, scrapped the buttons for now cause I could not get them to work. Now navigate through that part with arrow keys. More to come.

--- src/main/core/src/edu/udel/cisc275_15S/corndawgz/UDSIS.java
+++ src/main/core/src/edu/udel/cisc275_15S/corndawgz/UDSIS.java
@@ -30,26 +30,70 @@ public class UDSIS implements MyEvent {
 		startTime = TimeUtils.millis();
 		stage = new ImageSequence();
 		stage.addImages(new Image(new Texture(Gdx.files.internal("UDSIS/start.png"))));
+		stage.addImages(new Image(new Texture(Gdx.files.internal("UDSIS/advisorLocation.png"))));
 		stage.addImages(new Image(new Texture(Gdx.files.internal("UDSIS/register1.png"))));
 		stage.addImages(new Image(new Texture(Gdx.files.internal("UDSIS/register2.png"))));
 		stage.addImages(new Image(new Texture(Gdx.files.internal("UDSIS/register3.png"))));
+		stage.addImages(new Image(new Texture(Gdx.files.internal("UDSIS/change1.png"))));
+		stage.addImages(new Image(new Texture(Gdx.files.internal("UDSIS/change2.png"))));
 		stage.setFillParentTrue();
 		stage.update();
-	}
-
-	public void show(){
+		
 		skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
 		
 		nextButton = new TextButton("Next", skin);
 		nextButton.setColor(Color.BLUE);
 		nextButton.setWidth(BUTTON_WIDTH);
 		nextButton.setHeight(BUTTON_HEIGHT);
-		nextButton.setPosition(Gdx.graphics.getWidth() *3/4, 
-				Gdx.graphics.getHeight() * 7/8);
+		nextButton.setPosition(Gdx.graphics.getWidth() *5/8, 
+				Gdx.graphics.getHeight() * 1/16);
+//		nextButton.addListener(new ClickListener() {
+//			@Override
+//			public void clicked(InputEvent event, float x, float y) {
+//				if(TimeUtils.millis() > startTime + 1000){
+//					if(stage.hasNext()){
+//						stage.nextImage();
+//						startTime = TimeUtils.millis();
+//					}
+//					else{
+//						done = true;
+//					}
+//				}
+//			}
+//		});
+		
+		backButton = new TextButton("Back", skin);
+		backButton.setColor(Color.BLUE);
+		backButton.setWidth(BUTTON_WIDTH);
+		backButton.setHeight(BUTTON_HEIGHT);
+		backButton.setPosition(Gdx.graphics.getWidth() *13/16, 
+				Gdx.graphics.getHeight() * 1/16);
+//		nextButton.addListener(new ClickListener() {
+//			@Override
+//			public void clicked(InputEvent event, float x, float y) {
+//				if(TimeUtils.millis() > startTime + 1000){
+//					if(stage.hasPrev()){
+//						stage.prevImage();
+//						startTime = TimeUtils.millis();
+//					}
+//				}
+//			}
+//		});
+	}
+
+	public void show(){
+//		skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+//		
+//		nextButton = new TextButton("Next", skin);
+//		nextButton.setColor(Color.BLUE);
+//		nextButton.setWidth(BUTTON_WIDTH);
+//		nextButton.setHeight(BUTTON_HEIGHT);
+//		nextButton.setPosition(Gdx.graphics.getWidth() *5/8, 
+//				Gdx.graphics.getHeight() * 1/8);
 		nextButton.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				if(TimeUtils.millis() > startTime + 1000){
+//				if(TimeUtils.millis() > startTime + 1000){
 					if(stage.hasNext()){
 						stage.nextImage();
 						startTime = TimeUtils.millis();
@@ -57,28 +101,28 @@ public class UDSIS implements MyEvent {
 					else{
 						done = true;
 					}
-				}
+//				}
 			}
 		});
-		
-		backButton = new TextButton("Next", skin);
-		backButton.setColor(Color.BLUE);
-		backButton.setWidth(BUTTON_WIDTH);
-		backButton.setHeight(BUTTON_HEIGHT);
-		backButton.setPosition(Gdx.graphics.getWidth() *4/8, 
-				Gdx.graphics.getHeight() * 4/8);
+//		
+//		backButton = new TextButton("Back", skin);
+//		backButton.setColor(Color.BLUE);
+//		backButton.setWidth(BUTTON_WIDTH);
+//		backButton.setHeight(BUTTON_HEIGHT);
+//		backButton.setPosition(Gdx.graphics.getWidth() *7/8, 
+//				Gdx.graphics.getHeight() * 1/8);
 		nextButton.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				if(TimeUtils.millis() > startTime + 1000){
+//				if(TimeUtils.millis() > startTime + 1000){
 					if(stage.hasPrev()){
 						stage.prevImage();
 						startTime = TimeUtils.millis();
 					}
-				}
+//				}
 			}
 		});
-		
+//		
 		stage.addActor(nextButton);
 		stage.addActor(backButton);
 		Gdx.input.setInputProcessor(stage);
@@ -90,18 +134,28 @@ public class UDSIS implements MyEvent {
 		Gdx.gl.glClearColor(1,1,1,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+//		stage.addActor(nextButton);
+//		stage.addActor(backButton);
 		stage.draw();
 		batch.end();
 	}
 	
 	public void HandleInput(){
-		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-			if (TimeUtils.millis() > startTime + 1000) {
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			if (TimeUtils.millis() > startTime + 500) {
 				if (stage.hasNext()) {
 					stage.nextImage();
 					startTime = TimeUtils.millis();
 				} else {
 					done = true;
+				}
+			}
+		}
+		else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			if (TimeUtils.millis() > startTime + 500) {
+				if (stage.hasPrev()) {
+					stage.prevImage();
+					startTime = TimeUtils.millis();
 				}
 			}
 		}
